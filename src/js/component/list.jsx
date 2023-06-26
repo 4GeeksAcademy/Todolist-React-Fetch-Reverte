e78eded54f6d6dc2d93232058e5d13bb863c1f4d
Sergio Reverte <129745342+Reverte10@users.noreply.github.com>
2023-06-26 16:04:12 +0000

    Completado + borrado

--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -13,17 +13,28 @@ const List = () => {
 
     const añadirTarea = () => {
         if (tarea != "") {
-            setTareas([...tareas, tarea])
+            setTareas([...tareas, { label: tarea, done: false }])
             setTarea("")
         } else {
             alert("Debe rellenar la tarea")
         }
     }
 
-    const borrarTarea = (index) => {
-        const nuevasTareas = tareas.filter((tarea, i) => i != index);
+    const borrarTarea = (tarea) => {
+        const nuevasTareas = tareas.filter((item) => item.label != tarea);
         setTareas(nuevasTareas);
-      };
+    };
+
+    const completarTarea = (tarea) => {
+        const newArr = tareas.map((item) => {
+            if (item.label === tarea) {
+                return { label: item.label, done: true }
+            } else {
+                return item
+            }
+        })
+        setTareas(newArr)
+    }
 
     return (
         <div className="container bg-warning-subtle" style={{ width: "500px" }}>
@@ -33,14 +44,15 @@ const List = () => {
             <form>
                 <div className="mb-3">
                     <input type="text" onChange={handleTarea} className="form-control" id="form2" placeholder="Añadir tarea" value={tarea} />
-                    <ul className="list-group"> {tareas.map((item, index) => (<li key={index} className="list-group-item d-flex justify-content-between"> {item} <span className="delete-icon" onClick={() => borrarTarea(index)}><b>X</b></span></li>))}</ul>
+                    <ul className="list-group mt-4">{tareas.map((item, index) => <li key={index} className={`d-flex justify-content-between list-group-item ${item.done ? "text-decoration-line-through" : ""}`}>{item.label} <span onClick={() => completarTarea(item.label)}><b>C</b></span>
+                        <span className="delete-icon" onClick={() => borrarTarea(item.label)}><b>X</b></span></li>)}</ul>
                 </div>
             </form>
             <button type="button" onClick={añadirTarea} className="btn btn-secondary">New</button>
             <div className='text' id='integer'>
                 <footer>{tareas.length > 0 ? tareas.length + " items left" : ""}</footer>
             </div>
-           
+
         </div>
 
     );
